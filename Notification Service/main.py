3d0c5e79eb808fed06db07b75f6be54d58c88f9b
VeraMol06 <veramolotsova2006@gmail.com>
2026-09-14 14:15:52 +0300

    Update main.py

--- Notification Service/main.py
+++ Notification Service/main.py
@@ -1,1 +1,9 @@
+from fastapi import FASTAPI, Request
+from fastapi.responses import JSONResponse
+from pydnatic import BaseModel, ValidationError
+import logging
+import os
+'''Логирование в файл и в консоль'''
+LOG_FILE = "notofication.log"
 
+logger = logging.getLogger(""
